ceb09aa898c7dca156f60bb0368fd4335e6f4e3b
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-09 20:15:45 +0900

    4/9 A+B -7 수정

--- 반복문_3/7_A+B -7.py
+++ 반복문_3/7_A+B -7.py
@@ -6,6 +6,6 @@
 
 t = int(input())
 
-for i in range(1,t+1):
-    a,b = map(int,input().split())
-    print("case #"+str(i)+':',a+b)
+for i in range(1, t + 1):
+    a, b = map(int, input().split())
+    print(f'Case #{i}: {a + b}')
