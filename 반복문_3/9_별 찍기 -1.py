dd6192de32e4ef892889305b8c8df6d7efd194b2
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-09 20:45:08 +0900

    4/9 별찍기 -1

--- 반복문_3/9_별 찍기 -1.py
+++ 반복문_3/9_별 찍기 -1.py
@@ -3,3 +3,10 @@
 # 입력 : 첫쩨 줄에 N(1 <= N <= 100)이 주어진다.
 # 출력 : 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
 
+n = int(input())
+
+for i in range(n):
+    for j in range(i+1):
+        print('*', end='')
+    if i is not n-1:
+        print()
