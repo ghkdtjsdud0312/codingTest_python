7825e29627bed018ba2f6020d020202f05b657f0
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-10 22:59:16 +0900

    4/10 최댓값

--- 1차원 배열_4/4_최댓값.py
+++ 1차원 배열_4/4_최댓값.py
@@ -4,3 +4,9 @@
 # 입력 : 첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
 # 출력 : 첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
 
+num_list = []
+for i in range(9):
+    num_list.append(int(input()))
+
+print(max(num_list))
+print(num_list.index(max(num_list))+1)
