d2f8f68fd91071d538ca278056675433b91cd0f1
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-11 16:15:21 +0900

    4/11 평균 답

--- 1차원 배열_4/10_평균.py
+++ 1차원 배열_4/10_평균.py
@@ -5,3 +5,12 @@
 # 입력 : 첫째 줄에 시험 본 과목의 개수 N이 주어진다. 이 값은 1000보다 작거나 같다. 둘째 줄에 세준이의 현재 성적이 주어진다. 이 값은 100보다 작거나 같은 음이 아닌 정수이고, 적어도 하나의 값은 0보다 크다.
 # 출력 : 첫째 줄에 새로운 평균을 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10^-2 이하이면 정답이다.
 
+n = int(input())  # 과목 수
+test_list = list(map(int, input().split()))
+max_score = max(test_list)
+
+new_list = []
+for score in test_list :
+    new_list.append(score/max_score *100)  # 새로운 점수 생성
+test_avg = sum(new_list)/n
+print(test_avg)
