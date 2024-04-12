4cc26ef392d3c306a5f5f211b7ff502a97c5ead9
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-12 21:11:36 +0900

    4/12 알파벳 찾기 답

--- 문자열_5/6_알파벳 찾기.py
+++ 문자열_5/6_알파벳 찾기.py
@@ -4,3 +4,11 @@
 # 출력 : 각각의 알파벳에 대해서, a가 처음 등장하는 위치, b가 처음 등장하는 위치, ... z가 처음 등장하는 위치를 공백으로 구분해서 출력한다.
 # 만약, 어떤 알파벳이 단어에 포함되어 있지 않다면 -1을 출력한다. 단어의 첫 번째 글자는 0번째 위치이고, 두 번째 글자는 1번째 위치이다.
 
+s = list(input())
+c = 'abcdefghijklmnopqrstuvwxyz'
+
+for i in c:
+    if i in s:
+        print(s.index(i), end=' ')
+    else:
+        print(-1, end=' ')
