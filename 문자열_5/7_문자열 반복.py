6cd0e4f7dfb9d464aae9fd7df583ec04404a23da
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-13 22:31:51 +0900

    4/13 문자열 반복 답

--- 문자열_5/7_문자열 반복.py
+++ 문자열_5/7_문자열 반복.py
@@ -4,3 +4,12 @@
 # 입력 : 첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다. 각 테스트 케이스는 반복 횟수 R(1 ≤ R ≤ 8), 문자열 S가 공백으로 구분되어 주어진다. S의 길이는 적어도 1이며, 20글자를 넘지 않는다.
 # 출력 : 각 테스트 케이스에 대해 P를 출력한다.
 
+case = int(input())
+
+for i in range(case):
+    n,s = map(str,input().split())
+    n = int(n)
+    s = list(s)
+    for j in range(len(s)):
+        print(s[j] * n, end='')
+    print('')
