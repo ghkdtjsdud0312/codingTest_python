82fa346fd456cd1a607ea861fee84acf03cf27cf
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-14 23:32:59 +0900

    4/14 너의 평점은 답

--- 심화_6/8_너의 평점은.py
+++ 심화_6/8_너의 평점은.py
@@ -15,4 +15,18 @@
 # 등급은 A+,A0,B+,B0,C+,C0,D+,D0,F,P중 하나이다.
 # 적어도 한 과목은 등급이 P가 아님이 보장된다.
 
+rating = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
+grade = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
+
+total = 0
+result = 0
+
+for i in range(20):
+    s,p,g = input().split()
+    p = float(p)
+    if g != 'P':
+        total += p
+        result += p * grade[rating.index(g)]
+
+print('%.6f' % (result / total))
 
