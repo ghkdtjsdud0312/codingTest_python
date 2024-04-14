4e0a61c126c4ce97efcfa5bf0c6f768513da883c
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-14 23:27:07 +0900

    4/14 그룹 단어 체커 답

--- 심화_6/7_그룹 단어 체커.py
+++ 심화_6/7_그룹 단어 체커.py
@@ -4,3 +4,14 @@
 # 입력 : 첫째 줄에 단어의 개수 N이 들어온다. N은 100보다 작거나 같은 자연수이다. 둘째 줄부터 N개의 줄에 단어가 들어온다. 단어는 알파벳 소문자로만 되어있고 중복되지 않으며, 길이는 최대 100이다.
 # 출력 : 첫째 줄에 그룹 단어의 개수를 출력한다.
 
+n = int(input())
+cnt = n
+for i in range(n):
+    word = input()
+    for j in range(0, len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
+print(cnt)
