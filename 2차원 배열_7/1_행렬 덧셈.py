a33b5fbf124f767b21898e9a3921d44869e578b1
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-15 21:34:24 +0900

    4/15 행렬 덧셈 답

--- 2차원 배열_7/1_행렬 덧셈.py
+++ 2차원 배열_7/1_행렬 덧셈.py
@@ -3,3 +3,19 @@
 # 입력 : 첫째 줄에 행렬의 크기 N 과 M이 주어진다. 둘째 줄부터 N개의 줄에 행렬 A의 원소 M개가 차례대로 주어진다. 이어서 N개의 줄에 행렬 B의 원소 M개가 차례대로 주어진다. N과 M은 100보다 작거나 같고, 행렬의 원소는 절댓값이 100보다 작거나 같은 정수이다.
 # 출력 : 첫째 줄부터 N개의 줄에 행렬 A와 B를 더한 행렬을 출력한다. 행렬의 각 원소는 공백으로 구분한다.
 
+a,b = [], []
+
+n,m = map(int,input().split())
+
+for row in range(n):
+    row = list(map(int,input().split()))
+    a.append(row)
+
+for row in range(n):
+    row = list(map(int,input().split()))
+    b.append(row)
+
+for row in range(n):
+    for col in range(m):
+        print(a[row][col] + b[row][col], end=' ')
+    print()
