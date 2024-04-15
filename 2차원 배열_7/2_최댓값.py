463c20067a2f97d8e131a95563a1bc7b5c835fdd
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-15 21:40:49 +0900

    4/15 최댓값 답

--- 2차원 배열_7/2_최댓값.py
+++ 2차원 배열_7/2_최댓값.py
@@ -4,3 +4,16 @@
 # 입력 : 첫째 줄부터 아홉 번째 줄까지 한 줄에 아홉 개씩 수가 주어진다. 주어지는 수는 100보다 작은 자연수 또는 0이다.
 # 출력 : 첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 위치한 행 번호와 열 번호를 빈칸을 사이에 두고 차례로 출력한다. 최댓값이 두 개 이상인 경우 그 중 한 곳의 위치를 출력한다.
 
+table = [list(map(int,input().split())) for i in range(9)]
+
+max_num = 0
+max_row, max_col = 0, 0
+for row in range(9):
+    for col in range(9):
+        if max_num <= table[row][col]:
+            max_row = row + 1
+            max_col = col + 1
+            max_num = table[row][col]
+
+print(max_num)
+print(max_row, max_col)
