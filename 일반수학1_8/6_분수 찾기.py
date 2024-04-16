aea9af94d8f44badc81dda9160349a519973166c
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-16 20:18:05 +0900

    4/16 분수찾기 답

--- 일반수학1_8/6_분수 찾기.py
+++ 일반수학1_8/6_분수 찾기.py
@@ -5,3 +5,20 @@
 # 출력 : 첫째 줄에 분수를 출력한다.
 
 
+num = int(input())
+line = 1
+
+while num > line:
+    num -= line
+    line += 1
+
+# 짝수일경우
+if line % 2 == 0:
+    a = num
+    b = line - num + 1
+# 홀수일경우
+elif line % 2 == 1:
+    a = line - num + 1
+    b = num
+
+print(f'{a}/{b}')
