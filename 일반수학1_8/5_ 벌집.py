6664a5e37055b91f8999c4fb430ccedec0120db1
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-16 20:13:46 +0900

    4/16 벌집 답

--- 일반수학1_8/5_ 벌집.py
+++ 일반수학1_8/5_ 벌집.py
@@ -3,3 +3,11 @@
 # 입력 : 첫째 줄에 N(1 ≤ N ≤ 1,000,000,000)이 주어진다.
 # 출력 : 입력으로 주어진 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나는지 출력한다.
 
+n = int(input())
+
+nums_pileup = 1
+cnt = 1
+while n > nums_pileup:
+    nums_pileup += 6 * cnt
+    cnt +=1
+print(cnt)
