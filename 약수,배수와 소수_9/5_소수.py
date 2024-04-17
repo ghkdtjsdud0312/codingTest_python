4016a158a4904be837a5f67fe2ff3640226fc57d
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-17 22:23:34 +0900

    4/17 소수 답

--- 약수,배수와 소수_9/5_소수.py
+++ 약수,배수와 소수_9/5_소수.py
@@ -6,3 +6,22 @@
 # 출력 : M이상 N이하의 자연수 중 소수인 것을 모두 찾아 첫째 줄에 그 합을, 둘째 줄에 그 중 최솟값을 출력한다.
 # 단, M이상 N이하의 자연수 중 소수가 없을 경우는 첫째 줄에 -1을 출력한다.
 
+start_num = int(input())
+last_num = int(input())
+
+sosu_list = []
+for num in range(start_num, last_num + 1):
+    error = 0
+    if num > 1:
+        for i in range(2, num):  # 2부터 num-1까지
+            if num % i == 0:
+                error += 1
+                break  # 2부터 num-1까지 나눈 몫이 0이면 error가 증가하고 for문을 끝냄
+        if error == 0:
+            sosu_list.append(num)  # error가 없으면 소수리스트에 추가
+
+if len(sosu_list) > 0:
+    print(sum(sosu_list))
+    print(min(sosu_list))
+else:
+    print(-1)
