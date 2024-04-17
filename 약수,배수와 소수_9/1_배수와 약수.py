01a77c6bbe17671ee3ce27cae7f26a2785d3aa70
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-17 22:03:53 +0900

    4/17 배수와 약수 답

--- 약수,배수와 소수_9/1_배수와 약수.py
+++ 약수,배수와 소수_9/1_배수와 약수.py
@@ -10,3 +10,14 @@
 # 입력 : 입력은 여러 테스트 케이스로 이루어져 있다. 각 테스트 케이스는 10,000이 넘지않는 두 자연수로 이루어져 있다. 마지막 줄에는 0이 2개 주어진다. 두 수가 같은 경우는 없다.
 # 출력 : 각 테스트 케이스마다 첫 번째 숫자가 두 번째 숫자의 약수라면 factor를, 배수라면 multiple을, 둘 다 아니라면 neither를 출력한다.
 
+x,y = map(int,input().split())
+
+while (x,y) != (0,0):
+    if x % y == 0:
+        print("multiple")
+    elif y % x == 0:
+        print("factor")
+    else:
+        print("neither")
+
+    x,y = map(int,input().split())
