8657a56e465fcce60bd53ab79f9cb57f10263775
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-17 22:17:25 +0900

    4/17 약수들의 합 답

--- 약수,배수와 소수_9/3_약수들의 합.py
+++ 약수,배수와 소수_9/3_약수들의 합.py
@@ -9,3 +9,15 @@
 # 이때, 약수들은 오름차순으로 나열해야 한다.
 # n이 완전수가 아니라면 n is NOT perfect. 를 출력한다.
 
+while True:
+    n = int(input())
+    if n == -1: # 입력 값이 -1이면 반복문 종료
+        break;
+    arr = []
+    for i in range(1, n):
+        if n % i == 0:
+            arr.append(i)
+    if sum(arr) == n:
+        print(n, " = ", " + ".join(str(i) for i in arr), sep="")
+    else:
+        print(n, "is NOT perfect.")
