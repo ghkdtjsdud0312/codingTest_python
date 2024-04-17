84dfac04bdf0e2b09c626a9014f38dda2831ec01
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-17 22:21:04 +0900

    4/17 소수 찾기 답

--- 약수,배수와 소수_9/4_소수 찾기.py
+++ 약수,배수와 소수_9/4_소수 찾기.py
@@ -3,3 +3,16 @@
 # 입력 : 첫 줄에 수의 개수 N이 주어진다. N은 100이하이다. 다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다.
 # 출력 : 주어진 수들 중 소수의 개수를 출력한다.
 
+n = int(input())
+data = list(map(int, input().split()))
+count = 0
+
+for x in data:
+    for i in range(2, x + 1):
+        if x % i == 0:
+            if x == i:
+                count += 1
+
+            break
+
+print(count)
