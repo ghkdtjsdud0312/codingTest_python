32da929cea9a3459230ec8786acd1a394e3113e2
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-18 14:58:00 +0900

    4/18 대지 답

--- 기하:직사각형과 삼각형_10/5_대지.py
+++ 기하:직사각형과 삼각형_10/5_대지.py
@@ -6,5 +6,13 @@
 
 # 입력 : 첫째 줄에는 점의 개수 N (1 ≤ N ≤ 100,000) 이 주어진다. 이어지는 N 줄에는 각 점의 좌표가 두 개의 정수로 한 줄에 하나씩 주어진다. 각각의 좌표는 -10,000 이상 10,000 이하의 정수이다.
 # 출력 : 첫째 줄에 N 개의 점을 둘러싸는 최소 크기의 직사각형의 넓이를 출력하시오.
+import sys
 
-
+input = sys.stdin.readline
+x_lst = []
+y_lst = []
+for _ in range(int(input())) :
+    x, y = map(int, input().split())
+    x_lst.append(x)
+    y_lst.append(y)
+print((max(x_lst) - min(x_lst)) * (max(y_lst) - min(y_lst)))
