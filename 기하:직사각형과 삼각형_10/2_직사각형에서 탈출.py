1ef160464a56cafee086f7691b7693c1e5d230cf
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-18 14:48:54 +0900

    4/18 직사각형에서 탈출 답

--- 기하:직사각형과 삼각형_10/2_직사각형에서 탈출.py
+++ 기하:직사각형과 삼각형_10/2_직사각형에서 탈출.py
@@ -3,3 +3,11 @@
 # 입력 : 첫째 줄에 x, y, w, h가 주어진다.
 # 출력 : 첫째 줄에 문제의 정답을 출력한다.
 
+# 제한
+# 1 ≤ w, h ≤ 1,000
+# 1 ≤ x ≤ w-1
+# 1 ≤ y ≤ h-1
+# x, y, w, h는 정수
+
+x,y,w,h=map(int,input().split())
+print(min(x,y,w-x,h-y))
