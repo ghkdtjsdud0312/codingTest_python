a450108dd52f9d8a44f65c67d9685ada91f8dde3
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-22 22:19:51 +0900

    4/22 좌표 정렬하기 답

--- 정렬_13/7_좌표 정렬하기.py
+++ 정렬_13/7_좌표 정렬하기.py
@@ -3,3 +3,11 @@
 # 입력 : 첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다. (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
 # 출력 : 첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
 
+n = int(input())
+li=[]
+for i in range(n):
+    [a,b,] = map(int,input().split())
+    li.append([a.b])
+li.sort()
+for i in li:
+    print(i[0],i[1])
