5732a2c5826f40e636ac3a6d1aa3724499619107
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-22 16:09:32 +0900

    4/22 수 정렬하기2 답

--- 정렬_13/4_수 정렬하기2.py
+++ 정렬_13/4_수 정렬하기2.py
@@ -3,3 +3,14 @@
 # 입력 : 첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
 # 출력 : 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
 
+import sys
+
+n = int(input())
+num = []
+for i in range(n):
+    num.append(int(sys.stdin.readline()))
+
+num.sort()
+
+for i in num:
+    print(i)
