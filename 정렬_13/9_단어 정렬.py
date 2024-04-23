51bcf0df1b061e46662c74535dedea231e58c6ea
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-23 20:03:56 +0900

    4/23 단어정렬 답

--- 정렬_13/9_단어 정렬.py
+++ 정렬_13/9_단어 정렬.py
@@ -6,4 +6,13 @@
 # 입력 : 첫째 줄에 단어의 개수 N이 주어진다. (1 ≤ N ≤ 20,000) 둘째 줄부터 N개의 줄에 걸쳐 알파벳 소문자로 이루어진 단어가 한 줄에 하나씩 주어진다. 주어지는 문자열의 길이는 50을 넘지 않는다.
 # 출력 : 조건에 따라 정렬하여 단어들을 출력한다.
 
+n = int(input())
 
+words = [str(input()) for i in range(n)]
+
+words = list(set(words))
+words.sort()
+words.sort(key=len)
+
+for i in words:
+    print(i)
