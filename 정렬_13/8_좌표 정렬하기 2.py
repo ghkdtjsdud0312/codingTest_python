cd4cd4abcb06938402a1c33eda6c6d028b78d521
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-23 19:59:38 +0900

    4/23 좌표 정렬하기2 답

--- 정렬_13/8_좌표 정렬하기 2.py
+++ 정렬_13/8_좌표 정렬하기 2.py
@@ -3,3 +3,17 @@
 # 입력 : 첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다. (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
 # 출력 : 첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
 
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+array=[]
+for i in range(n):
+    x,y = map(int,input().split())
+    array.append([y,x])
+
+s_array = sorted(array)
+
+for y,x in s_array:
+    print(x,y)
