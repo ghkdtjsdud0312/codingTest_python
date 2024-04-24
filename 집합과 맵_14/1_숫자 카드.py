f562f4b7e281bec35e845343c7646c2eb1771681
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-24 18:47:15 +0900

    4/24 숫자 카드 답

--- 집합과 맵_14/1_숫자 카드.py
+++ 집합과 맵_14/1_숫자 카드.py
@@ -4,3 +4,19 @@
 # 셋째 줄에는 M(1 ≤ M ≤ 500,000)이 주어진다. 넷째 줄에는 상근이가 가지고 있는 숫자 카드인지 아닌지를 구해야 할 M개의 정수가 주어지며, 이 수는 공백으로 구분되어져 있다. 이 수도 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다
 # 출력 : 첫째 줄에 입력으로 주어진 M개의 수에 대해서, 각 수가 적힌 숫자 카드를 상근이가 가지고 있으면 1을, 아니면 0을 공백으로 구분해 출력한다.
 
+import sys
+
+N = int(sys.stdin.readline())
+cards = list(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline())
+checks = list(map(int, sys.stdin.readline().split()))
+
+_dict = {}  # 속도는 dictionary!
+for i in range(len(cards)):
+    _dict[cards[i]] = 0  # 아무 숫자로 mapping
+
+for j in range(M):
+    if checks[j] not in _dict:
+        print(0, end=' ')
+    else:
+        print(1, end=' ')
