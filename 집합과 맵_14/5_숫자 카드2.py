cafc481fd1b70b44152bdaef3fce26376b3fb834
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-24 18:58:22 +0900

    4/24 숫자카드 2 답

--- 집합과 맵_14/5_숫자 카드2.py
+++ 집합과 맵_14/5_숫자 카드2.py
@@ -4,3 +4,26 @@
 # 셋째 줄에는 M(1 ≤ M ≤ 500,000)이 주어진다. 넷째 줄에는 상근이가 몇 개 가지고 있는 숫자 카드인지 구해야 할 M개의 정수가 주어지며, 이 수는 공백으로 구분되어져 있다. 이 수도 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다.
 # 출력 : 첫째 줄에 입력으로 주어진 M개의 수에 대해서, 각 수가 적힌 숫자 카드를 상근이가 몇 개 가지고 있는지를 공백으로 구분해 출력한다.
 
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+card = list(map(int, input().split()))
+m = int(input())
+mine = list(map(int, input().split()))
+
+card.sort()
+
+dic = {}
+
+for x in card:
+  if x in dic :
+    dic[x] += 1
+  else:
+    dic[x] = 1
+
+for x in mine:
+  if x in dic:
+    print(dic[x], end=' ')
+  else:
+    print('0', end= ' ')
