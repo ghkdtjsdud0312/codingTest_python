b4946f54cd4096af8f9ecae297cc161d0bd9f95d
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-27 22:16:38 +0900

    4/27 다음 소수 답

--- 약수,배수와 소수2_15/5_다음 소수.py
+++ 약수,배수와 소수2_15/5_다음 소수.py
@@ -3,3 +3,22 @@
 # 입력 : 첫째 줄에 테스트 케이스의 개수가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고, 정수 n이 주어진다.
 # 출력 : 각각의 테스트 케이스에 대해서 n보다 크거나 같은 소수 중 가장 작은 소수를 한 줄에 하나씩 출력한다.
 
+import sys
+input=sys.stdin.readline
+n=int(input())
+def ch(x):
+    for i in range(2,int(x**0.5)+1):
+        if x%i==0:
+            return False
+    return True
+for i in range(n):
+    i=int(input())
+    while True:
+        if i==0 or i==1:
+            print(2)
+            break
+        if ch(i):
+            print(i)
+            break
+        else:
+            i+=1
