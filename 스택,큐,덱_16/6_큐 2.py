b5b3f9023a696a532ba9e9fcb49c9d3df8357c92
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-04-30 00:50:18 +0900

    4/30 큐2 답

--- 스택,큐,덱_16/6_큐 2.py
+++ 스택,큐,덱_16/6_큐 2.py
@@ -10,3 +10,33 @@
 # 입력 : 첫째 줄에 주어지는 명령의 수 N (1 ≤ N ≤ 2,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 명령이 하나씩 주어진다. 주어지는 정수는 1보다 크거나 같고, 100,000보다 작거나 같다. 문제에 나와있지 않은 명령이 주어지는 경우는 없다.
 # 출력 : 출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.
 
+import sys
+from collections import deque
+n = int(input())
+queue = deque([])
+for i in range(n):
+    com = sys.stdin.readline().split()
+    if com[0] == 'push':
+        queue.append(com[1])
+    elif com[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.popleft())
+    elif com[0] == 'size':
+        print(len(queue))
+    elif com[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif com[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+    elif com[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
