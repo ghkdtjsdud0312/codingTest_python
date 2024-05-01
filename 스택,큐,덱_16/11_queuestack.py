89f76f99be1225aa2109c16c6542beb598ac48a9
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-05-01 22:53:40 +0900

    5/1 queuestack 답

--- 스택,큐,덱_16/11_queuestack.py
+++ 스택,큐,덱_16/11_queuestack.py
@@ -20,3 +20,28 @@
 # 입력으로 주어지는 모든 수는 정수이다.
 # 출력 :수열 C의 원소를 차례대로 queuestack에 삽입했을 때의 리턴값을 공백으로 구분하여 출력한다.
 
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+N = int(input())
+sequence_A = list(map(int, input().split())) ## queue == 0, stack == 1
+sequence_B = list(map(int, input().split())) ## i번 자료구조에 들어있는 원소
+M = int(input())                             ## 삽입할 수열의 길이
+sequence_C = list(map(int, input().split())) ## 삽입할 수열
+
+## 스택은 무시하고 큐만 deque에 추가하기
+queue = deque([])
+for i in range(N):
+  if sequence_A[i] == 0:
+    queue.appendleft(sequence_B[i])
+else:
+  if queue == []:
+    print(*sequence_C)
+    sys.exit()
+
+## deque가 하나의 큐 처럼 작동
+for i in range(M):
+  queue.append(sequence_C[i])
+  print(queue.popleft(), end = " ")
