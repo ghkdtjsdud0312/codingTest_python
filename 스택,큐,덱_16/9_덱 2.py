d130ce5cd1f77a451caa272e07eb257c776192e9
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-05-01 22:42:15 +0900

    5/1 덱2 답

--- 스택,큐,덱_16/9_덱 2.py
+++ 스택,큐,덱_16/9_덱 2.py
@@ -14,3 +14,27 @@
 # 출력을 요구하는 명령은 하나 이상 주어진다.
 # 출력 : 출력을 요구하는 명령이 주어질 때마다 명령의 결과를 한 줄에 하나씩 출력한다.
 
+import sys
+from collections import deque
+n = int(input())
+
+dequeue = deque()
+for i in range(n):
+    num = list(map(int, sys.stdin.readline().strip().split()))
+    l = len(dequeue)
+    if num[0] == 1:
+        dequeue.appendleft(num[1])
+    elif num[0] == 2:
+        dequeue.append(num[1])
+    elif num[0] == 3:
+        print(dequeue.popleft() if l else -1)
+    elif num[0] == 4:
+        print(dequeue.pop() if l else -1)
+    elif num[0] == 5:
+        print(len(dequeue))
+    elif num[0] == 6:
+        print(0 if l else 1)
+    elif num[0] == 7:
+        print(dequeue[0] if l else -1)
+    elif num[0] == 8:
+        print(dequeue[-1] if l else -1)
