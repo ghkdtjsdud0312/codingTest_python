33e6f2d640f0009ccf6967817eb2b4cbd9ebf7a4
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-05-02 13:05:56 +0900

    5/2 이항계수1 답

--- 조합론_17/4_이항 계수1.py
+++ 조합론_17/4_이항 계수1.py
@@ -3,3 +3,12 @@
 # 입력 : 첫째 줄에 N과 K가 주어진다.(1<=N<=10, 0<=K<=N)
 # 출력 : (N 밑 K)를 출력한다.
 
+n, k = map(int,input().split())
+
+def factorial(num):
+    result = 1
+    for i in range(2, num+1):
+        result *= i
+    return result
+
+print(factorial(n) // (factorial(n-k) * factorial(k)))
